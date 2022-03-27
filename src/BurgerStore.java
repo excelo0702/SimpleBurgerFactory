5a13bc45c2990aa5830e7b88e50b3089308885cb
tanish1804 <tanish.yadav@salesforce.com>
2022-03-27 18:57:09 +0530

    Complex Burger Design

--- src/BurgerStore.java
+++ src/BurgerStore.java
@@ -3,22 +3,29 @@ import Burger.*;
 public class BurgerStore {
     public void orderBurger(String burgerType) {
 
-        SimpleBurgerFactory factory = new SimpleBurgerFactory();
-
-        IBurger burger = factory.createBurger(burgerType);
-        //This area is varying, so we are taking it out and putting it into SimpleBurgerFactory
-
-//        if(burgerType.equals("Veg")) {
-//            burger = new VegBurger();
-//        } else if( burgerType.equals("cheese")) {
-//            burger = new CheeseBurger();
-//        } else if( burgerType.equals("chicken")) {
-//            burger = new ChickenBurger();
-//        } else if(burgerType.equals("corn")) {
+        IBurger burger;
+        if(burgerType.equals("Veg")) {
+            burger = new VegBurger();
+        }
+        else if( burgerType.equals("cheese")) {
+            burger = new CheeseBurger();
+        }
+        else if( burgerType.equals("chicken")) {
+            burger = new ChickenBurger();
+        }
+//        else if(burgerType.equals("corn")) {
 //            burger = new CornBurger();
-//        } else {
-//            burger = new PlainBurger();
 //        }
+        else if(burgerType.equals("coco")){
+            burger = new CocoBurger();
+        } else if(burgerType.equals("picky")){
+            burger = new PickyBurger();
+        } else if(burgerType.equals("spinach")){
+            burger = new CocoBurger();
+        }
+        else {
+            burger = new PlainBurger();
+        }
         burger.description();
         // bake burger
         burger.bake();
